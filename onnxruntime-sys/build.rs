fcba08acd0eb8be174e35520de56c61b84755be5
David Justice <david@devigned.com>
2023-10-20 15:08:43 -0400

    x64 architecture rather that x64_86
    Signed-off-by: David Justice <david@devigned.com>

--- onnxruntime-sys/build.rs
+++ onnxruntime-sys/build.rs
@@ -320,7 +320,7 @@ impl OnnxPrebuiltArchive for Triplet {
             // onnxruntime-win-arm-1.15.0.zip
             // onnxruntime-win-arm64-1.15.0.zip
             // onnxruntime-linux-x64-1.15.0.tgz
-            // onnxruntime-osx-x64-1.15.0.tgz
+            // onnxruntime-osx-x64_86-1.15.0.tgz
             // onnxruntime-osx-arm64-1.15.0.tgz
             (Os::Windows, Architecture::X86, Accelerator::None)
             | (Os::Windows, Architecture::X64, Accelerator::None)
@@ -335,7 +335,7 @@ impl OnnxPrebuiltArchive for Triplet {
             )),
             // onnxruntime-win-gpu-x64-1.15.0.zip
             // Note how this one is inverted from the linux one next
-            (Os::Windows, Architecture::X86_64, Accelerator::Gpu) => Cow::from(format!(
+            (Os::Windows, Architecture::X64, Accelerator::Gpu) => Cow::from(format!(
                 "{}-{}-{}",
                 self.os.as_onnx_str(),
                 self.accelerator.as_onnx_str(),
@@ -343,7 +343,7 @@ impl OnnxPrebuiltArchive for Triplet {
             )),
             // onnxruntime-linux-x64-gpu-1.15.0.tgz
             // Note how this one is inverted from the windows one above
-            (Os::Linux, Architecture::X86_64, Accelerator::Gpu) => Cow::from(format!(
+            (Os::Linux, Architecture::X64, Accelerator::Gpu) => Cow::from(format!(
                 "{}-{}-{}",
                 self.os.as_onnx_str(),
                 self.arch.as_onnx_str(),
