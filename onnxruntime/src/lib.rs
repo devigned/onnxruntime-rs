e8b91b7eb31047b05dc69c2518c3d8150d00b6aa
David Justice <david@devigned.com>
2023-10-26 12:09:35 -0400

    debug
    Signed-off-by: David Justice <david@devigned.com>

--- onnxruntime/src/lib.rs
+++ onnxruntime/src/lib.rs
@@ -163,8 +163,10 @@ lazy_static! {
     static ref G_ORT_API: Arc<Mutex<AtomicPtr<sys::OrtApi>>> = {
         let base: *const sys::OrtApiBase = unsafe { sys::OrtGetApiBase() };
         assert_ne!(base, std::ptr::null(), "Failed to assert OrtApiBase ptr is not null.");
+        let version_ptr = unsafe { (*base).GetVersionString.unwrap()() };
+        let version = char_p_to_string(version_ptr).unwrap();
         let api = unsafe{ (*base).GetApi.unwrap()(sys::ORT_API_VERSION) };
-        assert_ne!(api, std::ptr::null(), "Failed to assert OrtApi ptr is not null.");
+        assert_ne!(api, std::ptr::null(), "Failed to assert OrtApi ptr is not null. (ORT Version: {})", version);
         Arc::new(Mutex::new(AtomicPtr::new(api as *mut sys::OrtApi)))
     };
 }
